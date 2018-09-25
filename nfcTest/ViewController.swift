8518b51477c64043ee46553116e7660946509c95
Claus Wolf <claus@mactopics.de>
2018-09-25 17:53:43 +0200

    Initial Commit

---
+++ nfcTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nfcTest
+//
+//  Created by Claus Wolf on 25.09.18.
+//  Copyright © 2018 Claus Wolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
